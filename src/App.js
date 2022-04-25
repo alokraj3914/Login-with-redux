dcebedb8bcd316d8aef5ad54ad52a08efe0dbc38
Alok Raj <57935764+alokraj3914@users.noreply.github.com>
2022-04-25 21:36:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Login from './Components/Login';
+import Logout from './Components/Logout';
+import "./App.css";
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+
+const App = () => {
+  const user = useSelector(selectUser);
+
+  return (
+    <div>
+      {user ? <Logout/>  : <Login/> }
+      </div>
+  )
+}
+
+export default App;
